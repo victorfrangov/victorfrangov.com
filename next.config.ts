f51c7bcd153db1ac50caec05312a5e14973e4df8
Victor Frangov <59508395+vict0rfr@users.noreply.github.com>
2026-01-31 17:41:44 -0500

    removed bad code

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,8 @@
 import { NextConfig } from "next";
 import createNextIntlPlugin from "next-intl/plugin";
-import withBundleAnalyzer from "@next/bundle-analyzer";
 
 const nextConfig: NextConfig = {};
 
 const withNextIntl = createNextIntlPlugin();
-const withAnalyzer = withBundleAnalyzer({
-  enabled: process.env.ANALYZE === "true",
-});
 
-export default withAnalyzer(withNextIntl(nextConfig));
+export default withNextIntl(nextConfig);
