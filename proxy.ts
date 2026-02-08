f7388bb9fd44e861ab4dd887b117ff7b55487d30
Victor Frangov <59508395+vict0rfr@users.noreply.github.com>
2026-02-08 18:07:48 -0500

    added log temp

--- proxy.ts
+++ proxy.ts
@@ -8,7 +8,7 @@ const intlMiddleware = createMiddleware({
 // Next.js 16 proxy convention: export a single default `proxy(request)`
 export default function proxy(request: Request) {
     const authHeader = request.headers.get("X-Custom-Auth")
-
+    console.log("Auth Header:", req.headers['your-custom-header-name']);
     if (authHeader !== process.env.CLOUDFLARE_SECRET_KEY) {
         return new Response("Unauthorized Access", { status: 401 })
     }
